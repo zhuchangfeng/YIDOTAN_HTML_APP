c24af37b279891a6af7bcb66ca0b04e744516efc
zhuchangfeng <652165177@qq.com>
2020-04-18 16:44:54 +0800

    all plugin entrance

---
+++ src/Plugin/index.js
@@ -0,0 +1,18 @@
+import Loading from '../Plugin/loading/index.js';
+import Message from '../Plugin/message/index.js';
+import MessageBox from '../Plugin/message-box/index.js';
+
+const install = (Vue, opts = {}) => {
+    Vue.use(Loading.directive);
+    Vue.prototype.$message = Message;
+    Vue.prototype.$msgbox = MessageBox;
+}
+
+
+export default {
+    version: '2.13.0',
+    install,
+    Loading,
+    Message,
+    MessageBox
+}
