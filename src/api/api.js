9f4340f0c948dac6edeeb1c444c0e423af6f080b
zhuchangfeng <652165177@qq.com>
2020-04-18 16:52:47 +0800

    api unified entrance

---
+++ src/api/api.js
@@ -0,0 +1,69 @@
+import request from '../service/request';
+import qs from 'qs';
+
+function jsonProp(obj) {
+    // type check
+    if (!obj || (typeof obj !== 'object')) {
+        return obj;
+    }
+    Object.keys(obj).forEach((key) => {
+        if ((typeof obj[key]) === 'object') {
+            obj[key] = JSON.stringify(obj[key])
+        }
+    });
+    return obj;
+}
+
+function createObj(obj) {
+    const toString = Object.prototype.toString;
+    if (toString.call(obj) == "[object Object]") {
+        const { params, data, ...other } = obj;
+        const back = Object.assign(
+            data ? {
+                data: toString.call(data) == "[object Object]" ?
+                    qs.stringify(jsonProp(data)) : jsonProp(data)
+            } : params ? { params: params } : {}, other);
+        return back;
+    }
+    return {};
+}
+
+export const getInfo = (obj) => request({
+    method: "GET",
+    url: "/info",
+    ...createObj(obj)
+});
+
+export const getNewDetail = (obj) => request({
+    method: "GET",
+    url: "/new/detail",
+    ...createObj(obj)
+});
+
+export const getMasterDetail = (obj) => request({
+    method: "GET",
+    url: "/master/detail",
+    ...createObj(obj)
+});
+
+export const getZhongceList = (obj) => request({
+    method: "GET",
+    url: "/zhongce/list",
+    ...createObj(obj)
+});
+export const getZhongDetail = (obj) => request({
+    method: "GET",
+    url: "/zhongce/detail",
+    ...createObj(obj)
+});
+export const getSimilarList = (obj) => request({
+    method: "GET",
+    url: "/similar/list",
+    ...createObj(obj)
+});
+
+export const getDecorateList = (obj) => request({
+    method: "GET",
+    url: "/master/decorate",
+    ...createObj(obj)
+})
