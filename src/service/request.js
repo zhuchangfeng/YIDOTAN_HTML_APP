2f6f42361a53819fb949b1ddca6b432d8190f10f
zhuchangfeng <652165177@qq.com>
2020-04-28 17:37:38 +0800

    add service config

--- src/service/request.js
+++ src/service/request.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import config from './config';
 import _ from "lodash";
+import Vue from 'vue';
+const instance = new Vue('vue');
 //创建axios
 const request = axios.create(config);
 //默认请求带参数
 const urlPrefix = "/api";
-
+// 默认请求次数
+axios.defaults.retry = 4;
+// 默认请求的间隙
+axios.defaults.retryDelay = 1000;
+// 默认请求头
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // 响应拦截器拦截返回的响应信息 6100状态码为用户未登录
 request.interceptors.response.use(
@@ -31,7 +37,42 @@ request.interceptors.response.use(
         return response;
     },
     error => {
-        return Promise.reject(error);
+        //请求超时的之后，抛出 error.code = ECONNABORTED的错误..错误信息是 timeout of  xxx ms exceeded
+        if (error.code == 'ECONNABORTED' && error.message.indexOf('timeout') != -1) {
+            const config = error.config;
+            if (!config.retry) {
+                config.retry = axios.defaults.retry;
+            }
+            if (!config.retryDelay) {
+                config.retryDelay = axios.defaults.retryDelay;
+            }
+            // If config does not exist or the retry option is not set, reject
+            if (!config || !config.retry) return Promise.reject(error);
+            config.__retryCount = config.__retryCount || 0;
+            // Reject with the error
+            if (config.__retryCount >= config.retry) {
+                return Promise.reject(error);
+            };
+            // Increase the retry count
+            config.__retryCount += 1;
+            // Create new promise to handle exponential backoff
+            let backoff = new Promise(function(resolve) {
+                setTimeout(function() {
+                    resolve();
+                    instance.$message({
+                        message: "请求超时"
+                    });
+                }, config.retryDelay || 1);
+            });
+            // Return the promise in which recalls axios to retry the request
+            return backoff.then(() => axios(config));
+        } else {
+            const { status } = error.request;
+            instance.$message({
+                message: `服务错误${status}`
+            });
+            return Promise.reject(error);
+        }
     });
 // 添加请求拦截器
 request.interceptors.request.use(function(option) {
