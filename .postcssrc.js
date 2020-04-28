3a5ac64d4557335d39018e1d28cb01d43d3dab4f
zhuchangfeng <652165177@qq.com>
2020-04-28 17:41:44 +0800

    rem config

--- .postcssrc.js
+++ .postcssrc.js
@@ -8,7 +8,7 @@ module.exports = {
         "autoprefixer": {},
         "postcss-plugin-px2rem": {
             rootValue: 37.5, //换算基数， 默认100 
-            unitPrecision: 6, //允许REM单位增长到的十进制数字。
+            unitPrecision: 8, //允许REM单位增长到的十进制数字。
             // propWhiteList: ["font-size"],  //默认值是一个空数组，这意味着禁用白名单并启用所有属性。
             propBlackList: ["border-radius", "box-shadow"], //黑名单
             exclude: /(node_module)/, //默认false，可以（reg）利用正则表达式排除某些文件夹的方法，例如/(node_module)/ 。如果想把前端UI框架内的px也转换成rem，请把此属性设为默认值
